8c22efa48a3ff2824d2ae7ebae2ad5475706d679
ghostmuji <saraht789@yahoo.com>
2024-09-16 18:48:12 -0400

    Repo retrieved from pi on Mon Sep 16 06:48:12 PM EDT 2024

---
+++ raspberrypi/config-repo/sys_update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Directory where the IP file and configs will be saved
+DIR="$HOME/cs334/raspberrypi"
+FILE="$DIR/ip.md"
+CONFIG_DIR="$HOME/cs334/raspberrypi/config-repo"
+SCRIPT_PATH="$HOME/cs334/.scripts/sys-update.sh"
+
+# make directory
+mkdir -p $DIR
+mkdir -p $CONFIG_DIR
+
+# copy IP address to ip.md FILE
+IP=$(hostname -I)
+echo "Current IP Address: $IP" > $FILE
+
+# copy important config files to config-repo
+cp $HOME/.config/wayfire.ini $CONFIG_DIR/wayfire.ini
+cp $SCRIPT_PATH $CONFIG_DIR/sys_update.sh
+
+cd $DIR
+git add ip.md
+git add $CONFIG_DIR
+git add ps5-test
+git commit -m "Repo retrieved from pi on $(date)"
+git push origin main
